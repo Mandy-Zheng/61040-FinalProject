59f1cdf1fb2e20dff0bb238e86aed2cef069b03e
Amanda Zheng <ayzheng@mit.edu>
2023-11-29 10:45:00 -0500

    fixed add member functionality to only appear if user is admin

--- client/components/Organization/OrganizationComponent.vue
+++ client/components/Organization/OrganizationComponent.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import AddMemberComponent from "@/components/Organization/AddMemberComponent.vue";
+import { useUserStore } from "@/stores/user";
+import { storeToRefs } from "pinia";
 import { onBeforeMount, ref } from "vue";
 
+const { currentUsername } = storeToRefs(useUserStore());
 const props = defineProps(["organization"]);
-const showEditModal = ref<boolean>(false);
+const showAddModal = ref<boolean>(false);
 onBeforeMount(() => {
   console.log("WOAH");
 });
@@ -16,10 +19,12 @@ onBeforeMount(() => {
     <div v-for="admin in props.organization.admins" :key="admin">{{ admin }}</div>
     <p>Members</p>
     <div v-for="member in props.organization.members" :key="member">{{ member }}</div>
-    <button class="pure-button" @click.prevent="showEditModal = true">Edit</button>
-    <teleport to="body">
-      <AddMemberComponent :show="showEditModal" :organization="organization" @close="showEditModal = false" />
-    </teleport>
+    <div v-if="props.organization.admins.includes(currentUsername)">
+      <button class="pure-button" @click.prevent="showAddModal = true">Add Member</button>
+      <teleport to="body">
+        <AddMemberComponent :show="showAddModal" :organization="organization" @close="showAddModal = false" />
+      </teleport>
+    </div>
   </div>
   <section></section>
 </template>
