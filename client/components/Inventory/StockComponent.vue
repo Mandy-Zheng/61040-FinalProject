77e0c3d5b597b7442575a6b65bcf8d2c7d1b956c
Amanda Zheng <ayzheng@mit.edu>
2023-11-30 20:29:42 -0500

    started inventory

---
+++ client/components/Inventory/StockComponent.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { fetchy } from "@/utils/fetchy";
+import { onBeforeMount } from "vue";
+
+const props = defineProps(["stockId"]);
+
+async function getInventory() {
+  inventory.value = await fetchy(`/api/inventory`, "GET");
+}
+
+onBeforeMount(async () => {
+  await getInventory();
+});
+</script>
+
+<template>
+  <main>
+    <p>Stock</p>
+    <div>{{ props.stockId }}</div>
+  </main>
+</template>
--- client/views/InventoryView.vue
+++ client/views/InventoryView.vue
@@ -1,5 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import StockComponent from "@/components/Inventory/StockComponent.vue";
+import { useOrganizationStore } from "@/stores/organization";
+import { fetchy } from "@/utils/fetchy";
+import { storeToRefs } from "pinia";
+import { onBeforeMount, ref } from "vue";
+const { selectedOrg } = storeToRefs(useOrganizationStore());
+const inventory = ref<Array<any>>([]);
+
+async function getInventory() {
+  try {
+    if (selectedOrg.value) {
+      inventory.value = await fetchy(`/api/inventory/${selectedOrg.value.id}`, "GET");
+    }
+  } catch (error) {
+    return;
+  }
+}
+
+onBeforeMount(async () => {
+  await getInventory();
+});
+</script>
 
 <template>
-  <main>Inventory</main>
+  <main>
+    <h3>Inventory</h3>
+    <div v-for="stock in inventory" :key="stock"><StockComponent :stock="stock._id" /></div>
+  </main>
 </template>
