09b88fbcccab0ad9a949a8fc0283d22d9851fc03
Andrew <lightningtrackmc@gmail.com>
2023-12-06 00:51:27 -0500

    minor test

--- client/components/Patron/HouseholdComponent.vue
+++ client/components/Patron/HouseholdComponent.vue
@@ -41,7 +41,7 @@ const addVisit = async () => {
         </div>
       </div>
       <div class="modify">
-        <button class="icon" @click.prevent="showDeleteModal = true" v-b-tooltip.hover title="Delete shift">
+        <button class="icon" @click.prevent="showDeleteModal = true" v-b-tooltip.hover title="Delete household">
           <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="black" class="bi bi-trash-fill" viewBox="0 0 16 16">
             <path
               d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"
