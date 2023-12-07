54c87609f00d9658959cdac725bc87aa54ed761a
Anna <azhang03@mit.edu>
2023-12-06 22:30:43 -0500

    now fix?

--- client/components/Inventory/CreateStockModal.vue
+++ client/components/Inventory/CreateStockModal.vue
@@ -32,7 +32,7 @@ onBeforeMount(async () => {});
   <transition name="modal">
     <div v-if="props.show" class="modal-mask">
       <div class="modal-container">
-        <form>
+        <form @submit.prevent="emit('add', name, imgLink, purchaseLink, units, diet, maxPerPerson), emptyForm()">
           <h1>Create New Inventory</h1>
           <div class="form">
             <img v-if="!imgLink.length" src="../../assets/images/image.svg" />
@@ -52,7 +52,7 @@ onBeforeMount(async () => {});
           </div>
           <div class="modal-footer">
             <button class="button-39" @click="emit('close'), emptyForm()">Cancel</button>
-            <button class="button-39" type="submit" @click="emit('add', name, imgLink, purchaseLink, units, diet, maxPerPerson), emptyForm()">Add</button>
+            <button class="button-39" type="submit">Add</button>
           </div>
         </form>
       </div>
--- client/components/Inventory/StockComponent.vue
+++ client/components/Inventory/StockComponent.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
 import { BodyT, fetchy } from "@/utils/fetchy";
-import { storeToRefs } from "pinia";
 import { onBeforeMount, ref } from "vue";
-import { useOrganizationStore } from "../../stores/organization";
 import DeleteStockModal from "./DeleteStockModal.vue";
 import EditStockModal from "./EditStockModal.vue";
 
-const { selectedOrg } = storeToRefs(useOrganizationStore());
 const props = defineProps(["stockId"]);
 const item = ref();
 const emit = defineEmits(["refreshStocks"]);
@@ -88,7 +85,7 @@ onBeforeMount(async () => {
     </div>
     <div class="row" style="gap: 1em">
       <div class="link">
-        <a :href="item.supplyLink" v-if="item.supplyLink"
+        <a :href="item.supplyLink" target="_blank" v-if="item.supplyLink"
           ><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" class="bi bi-cart-fill" viewBox="0 0 16 16">
             <path
               d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"
