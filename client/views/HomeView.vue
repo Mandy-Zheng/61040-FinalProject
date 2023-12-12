7479128a6a4c8b5cc2f534634d1311e72709d077
Anna <azhang03@mit.edu>
2023-12-12 15:20:43 -0500

    more info to homepage

--- client/views/HomeView.vue
+++ client/views/HomeView.vue
@@ -17,6 +17,33 @@
         insecurity in 2020. To address these challenges, we present Cornucopia, an app that helps streamline food pantry operations.
       </p>
     </div>
+    <div class="subinfo">
+      <p>Some features that Cornucopia offers are</p>
+      <ul>
+        <li>smart inventory tracking and algorithmic resource allocation</li>
+        <li>individualized household management</li>
+        <li>support for audio and text translational accommodation</li>
+        <li>integrated volunteer scheduling services</li>
+        <li>administrative organization management tools</li>
+      </ul>
+    </div>
+    <div class="users">
+      <h3>What are you waiting for?</h3>
+      <p>If you are an organization administrator, create an account with Cornucopia, register your pantry, and get started!</p>
+      <p>If you are looking to volunteer at an organization that uses Cornucopia, create an account and ask an admin to add you!</p>
+    </div>
+    <div class="about">
+      <h3>About us</h3>
+      <p>
+        We are 3rd and 4th-year computer science undergraduates at MIT, currently taking Software Design (6.1040), tasked with creating an authentic app that addresses an important problem domain for
+        our final project. We were initially inspired to create Cornucopia during an interview with Phoebe Heyman, the coordinator of the Margaret Fuller House Food Pantry; we saw many opportunities
+        to digitize processes at these important organizations and hope that our app will provide a more efficient, streamlined, centralized, and accommodative experience for administrators,
+        volunteers, and patrons.
+      </p>
+    </div>
+    <footer>
+      <small> Questions or feedback? Contact us at aaap at mit dot edu. </small>
+    </footer>
   </main>
 </template>
 
@@ -68,7 +95,33 @@ h1 {
   justify-content: flex-start;
   width: 35em;
   font-size: x-large;
-  padding: 2em;
+  padding: 2em 2em 0 2em;
+}
+
+.subinfo {
+  width: 35em;
+  font-size: large;
+  padding-left: 5em;
+}
+
+.users {
+  width: 35em;
+  font-size: x-large;
+  padding-left: 2em;
+  padding-top: 2em;
+}
+
+.about {
+  width: 50em;
+  font-size: medium;
+  padding-left: 3em;
+  padding-top: 2em;
+}
+
+footer {
+  padding: 10px;
+  padding-left: 3em;
+  font-style: italic;
 }
 
 a {
